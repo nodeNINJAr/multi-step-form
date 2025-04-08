a8397a710957862ea676e20b9c491d5a73e85730
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-04-08 23:53:29 +0600

    project-setup

---
+++ app/multi-step-form/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    // 
+  return (
+    <div>
+        <h2 className='text-4xl text-red-500'>hello form form page</h2>
+    </div>
+  )
+}
+
+export default page
